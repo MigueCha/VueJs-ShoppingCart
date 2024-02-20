aa5cca7a676020e10348f58f64f84dfa08770650
Miguel Chavez <chavezmendoza727@gmail.com>
2024-02-20 14:55:46 -0600

    🌟Feat: Author Name Create the Author name Component

---
+++ src/components/AuthorName.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1> Author Name: </h1>
+        <p> <a target="_blank" href="https://github.com/MigueCha "> Miguel Chavez
+        </a>
+    </p>
+    </div>
+</template>
