6bf27089f75ae4dc427c5b43adcbf24702bd4b08
Miguel Chavez <chavezmendoza727@gmail.com>
2024-03-01 11:49:36 -0600

    🌟FEAT: List Rendering Add a list of items using v-for

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,21 @@ import { ref } from 'vue'
 const header = ref('App lista de compras');
 const shoppingIcon = ref('material-icons shopping-cart-icon');
 //const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
+//Creando una referencia reactiva para almacenar el valor de la lista
+const items = ref([
+{id: 0, label: 'Leche'},
+{id: 1, label: 'Arroz'},
+{id: 2, label: 'Carne'},
+{id: 3, label: 'Pan'},
+{id: 4, label: 'Huevos'}
+]);
 </script>
 
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.id">🌟 {{ item.label }}</li>
+  </ul>
 </template>
 
 <style scoped>
