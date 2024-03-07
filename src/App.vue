d3a764f52ac42968a8c444cb0c93f26466b78481
Miguel Chavez <chavezmendoza727@gmail.com>
2024-03-07 12:01:28 -0600

    🌟FEAT: Checkboxes Bind checkboxes to newItemPriority

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,29 @@
 import { ref } from 'vue'
 //Creando una referencia reactiva de tipo string
 const header = ref('App lista de compras');
-const items = ref([
-  {id: 1, label: '10 Bolillos'},
-  {id: 2, label: '1 Lata de Frijoles'},
-  {id: 3, label: '1 Lata de Atun'}
-]);
-//const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
+const shoppingIcon = ref("material-icons shopping-cart-icon")
 //Creando una referencia reactiva para almacenar el valor de la lista
-/*const items = ref([
+const items = ref([
 {id: 0, label: 'Leche'},
 {id: 1, label: 'Arroz'},
 {id: 2, label: 'Carne'},
 {id: 3, label: 'Pan'},
 {id: 4, label: 'Huevos'}
-]);*/
+]);
+
+const newItem = ref('');
+const newItemPriority = ref('low');
 </script>
 
 <template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i>{{ header }}</h1>
+  <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <input v-model ="newItem" type="text" placeholder="Agregar Articulo">
+  <!-- Radio Buttons -->
+  <label><input type="radio" v-model="newItemPriority" value="low">Baja</label>
+  <label><input type="radio" v-model="newItemPriority" value="high">Alta</label>
+  {{ newItemPriority ==='low'?'🧊': '🔥' }}
   <ul>
-    <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{ label }}</li>
+    <li v-for="({ id, label }, i) in items" v-bind:key="id">🌟 {{ i }} {{ label }}</li>
   </ul>
 </template>
 
