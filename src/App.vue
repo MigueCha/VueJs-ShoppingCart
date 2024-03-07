586664b9819ce5020d6e6f6685ecd56314b430d4
Miguel Chavez <chavezmendoza727@gmail.com>
2024-03-07 10:54:27 -0600

    🌟 Listas 2

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,26 @@
 import { ref } from 'vue'
 //Creando una referencia reactiva de tipo string
 const header = ref('App lista de compras');
-const shoppingIcon = ref('material-icons shopping-cart-icon');
+const items = ref([
+  {id: 1, label: '10 Bolillos'},
+  {id: 2, label: '1 Lata de Frijoles'},
+  {id: 3, label: '1 Lata de Atun'}
+]);
 //const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
 //Creando una referencia reactiva para almacenar el valor de la lista
-const items = ref([
+/*const items = ref([
 {id: 0, label: 'Leche'},
 {id: 1, label: 'Arroz'},
 {id: 2, label: 'Carne'},
 {id: 3, label: 'Pan'},
 {id: 4, label: 'Huevos'}
-]);
+]);*/
 </script>
 
 <template>
-  <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i>{{ header }}</h1>
   <ul>
-    <li v-for="item in items" v-bind:key="item.id">🌟 {{ item.label }}</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{ label }}</li>
   </ul>
 </template>
 
